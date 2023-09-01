844ca3051a5a62134f28fd5e551614e7d796d88c
PabloZalem <zalem.pablo@yahoo.com>
2023-09-01 20:14:18 -0300

    Conceito de lambda

--- RevisaoDeLambdas/src/com/zalempablo/revisaolambdas/test/LambdasTest001.java
+++ RevisaoDeLambdas/src/com/zalempablo/revisaolambdas/test/LambdasTest001.java
@@ -9,7 +9,7 @@ public class LambdasTest001 {
 		//(parametro) -> <expressao>
 		//(Carro carro) -> carro.getCor().equals("verde");
 		
-		List<Integer> integer =  List.of(1,2,3,4,5,6,7,8);
+		List<Integer> integer=  List.of(1,2,3,4,5,6,7,8);
 		List<String> strings = List.of("Pablo", "Pedro", "Pietro");
 		forEach(strings, (String s) -> System.out.println(s));
 		forEach(integer, (Integer i) -> System.out.println(i));
